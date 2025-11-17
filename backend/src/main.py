9049f10718c5fd0557f4a301690e6c11951b8e4f
Ivan Fedorov <ivan45sc@gmail.com>
2025-11-17 20:51:04 +0500

    Include reports router in main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from src.reports.router import reports_router
 from src.requests.router import request_router
 from src.users.router import users_router
 
@@ -9,3 +10,4 @@ app = FastAPI()
 
 app.include_router(users_router)
 app.include_router(request_router)
+app.include_router(reports_router)
