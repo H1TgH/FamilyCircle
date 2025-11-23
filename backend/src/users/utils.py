c16c0ad7b198bc24aa1c382d4b1cf92c966347d9
Ivan Fedorov <ivan45sc@gmail.com>
2025-11-23 22:42:21 +0500

    Add function to get avatar presigned_ur

--- backend/src/users/utils.py
+++ backend/src/users/utils.py
@@ -6,7 +6,8 @@ import jwt
 from passlib.context import CryptContext
 
 from src.config import config
-from src.users.models import RoleEnum
+from src.minio import MinioClient
+from src.users.models import RoleEnum, UserModel
 
 
 password_context = CryptContext(schemes=['bcrypt'])
@@ -55,3 +56,23 @@ def decode_token(token: str) -> dict[str, Any]:
         raise ValueError('Token has expired') from e
     except jwt.InvalidTokenError as e:
         raise ValueError('Invalid token') from e
+
+
+async def get_avatar_presigned_url(user: UserModel) -> str | None:
+    if not user.is_has_avatar:
+        return None
+
+    bucket_name = config.minio.define_buckets['avatars']
+    if not bucket_name:
+        return None
+
+    minio_client = MinioClient(bucket_name=bucket_name)
+
+    async with minio_client.get_client() as client:
+        url = await client.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': bucket_name, 'Key': f'user_{user.id}'},
+            ExpiresIn=600
+        )
+
+        return url
